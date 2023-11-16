b3a8b34ec38b644769a0d3be09ff43d17c9d1ed8
Peter <petrucho9@gmail.com>
2023-11-16 03:52:20 +0300

    fix server port to refresh

--- low_level_network_app/main.py
+++ low_level_network_app/main.py
@@ -41,6 +41,7 @@ def generate_response(request):
 
 def run():
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     server_socket.bind(('localhost', 5000))
     server_socket.listen()
 
